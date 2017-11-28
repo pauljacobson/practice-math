f54ba700b71e76baebdcac9155c8c9c48b62fff8
pauljacobson <pljcbsn@gmail.com>
2017-11-28 16:42:27 +0200

    Initial random value generation

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,13 +1,26 @@
 // 1. Insert exercises into the page HTML
-const categoryContainer = (document.querySelector('.cat1--container'));
-console.log(categoryContainer);
+const container = (document.querySelector('.container'));
+const num1 = document.querySelector('.num1');
+const num2 = document.querySelector('.num2');
+let operator = document.querySelector('.operator');
+const operators = ['+', '-', '*', '/'];
 
-const example1 = {
-  var1: 24,
-  var2: 6,
-  var3: 4
-};
+// 2. Calculate random numbers
 
-categoryContainer.innerHTML = `<p>${example1.var1} / ${example1.var2} = ${example1.var3}</p>`;
+function randomNum(min, max) {
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min) -min);
+}
 
-// 2. Derive equations from an object containing exercise variables
+// 3. Randomly select an operator
+
+
+// 4. Populate the fields with randomly generated values
+num1.textContent = randomNum(10, 999);
+num2.textContent = randomNum(10, 999);
+
+// 5. Randomly generate an operator but don't allow operators that lead to negative results
+operator.textContent = operators[Math.floor(Math.random()*operators.length)];
+
+// 5. Calculate the value of each randomly generated equation
