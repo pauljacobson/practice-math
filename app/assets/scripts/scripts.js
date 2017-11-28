9dab60a3b163d22ad4bf456ac247a06f58039e9b
pauljacobson <pljcbsn@gmail.com>
2017-11-28 19:26:49 +0200

    Equation Generation 2.0

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -1,7 +1,6 @@
 const container = (document.querySelector('.container'));
-const num1 = document.querySelector('.num1');
-const num2 = document.querySelector('.num2');
-let operator = document.querySelector('.operator');
+const eq = document.querySelector('.equation');
+let operator;
 const btnNext = document.querySelector('#btn_next');
 const operators = ['+', '-', '*'];
 // const solutions = JSON.parse(localStorage.getItem('solutions')) || [];
@@ -15,24 +14,29 @@ function randomNum(min, max) {
 }
 
 // 2. Randomly select an operator
-operator.textContent = operators[Math.floor(Math.random()*operators.length)];
 
 // 4. Populate the fields with randomly generated values
 function calcValues() {
-  num1.textContent = randomNum(10, 999);
-  num2.textContent = randomNum(10, 999);
-  console.log(num1.textContent, num2.textContent);
-  // If num1 is smaller than num2, only select the `+` or `*` operators
-  if (parseInt(num1.textContent) < parseInt(num2.textContent)) {
-    console.log(`num1: ${num1.textContent}, num2: ${num2.textContent}`);
-  } else {
-    return;
-  }
+  const num1 = randomNum(10, 999);
+  const num2 = randomNum(10, 999);
+  eq.textContent = `
+  ${num1} ${operators[Math.floor(Math.random()*operators.length)]} ${num2}`;
 }
+
+// num1.textContent = randomNum(10, 999);
+// num2.textContent = randomNum(10, 999);
+// console.log(num1.textContent, num2.textContent);
+// // If num1 is smaller than num2, only select the `+` or `*` operators
+// if (parseInt(num1.textContent) < parseInt(num2.textContent)) {
+//   console.log(`num1: ${num1.textContent}, num2: ${num2.textContent}`);
+// } else {
+//   return;
+// }
 
 calcValues();
 
 // 5. Randomly generate an operator but don't allow operators that lead to negative results
+// Function to create operator that we then call in calcValues() for the operator
 
 // 5. Calculate the value of each randomly generated equation
 
