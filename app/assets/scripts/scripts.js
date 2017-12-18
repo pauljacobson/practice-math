f089988f18e8810a3de6df2f860b32bc3d3be6e4
Paul Jacobson <pljcbsn@gmail.com>
2017-12-18 18:32:17 +0200

    Added in mechanism to replace rendered operators with human friendly versions

--- app/assets/scripts/scripts.js
+++ app/assets/scripts/scripts.js
@@ -2,7 +2,7 @@ const container = (document.querySelector('.container'));
 const eq = document.querySelector('.equation');
 let operator;
 const btnNext = document.querySelector('#btn_next');
-const operators = ['+', '-', '*'];
+const operators = ['+', '-', '*', '/'];
 // const solutions = JSON.parse(localStorage.getItem('solutions')) || [];
 
 // 1. Calculate random numbers
@@ -12,34 +12,36 @@ function randomNum(min, max) {
 }
 
 // 2. Randomly select an operator
-const randomOperator = operators[Math.floor(Math.random()*operators.length)];
-console.log(randomOperator);
+function selectOperator() {
+  const randomOperator = operators[Math.floor(Math.random()*operators.length)];
+  if (randomOperator === '+') {
+    operator = '\u002B';
+  } else if (randomOperator === '-') {
+    operator = '\u002D';
+  } else if (randomOperator === '*') {
+    operator = '\u00D7';
+  } else if (randomOperator === '/') {
+    operator = '\u00F7';
+  }
+}
+
 
 // 4. Populate the fields with randomly generated values
-function calcValues() {
+function renderEquation() {
+  selectOperator();
   const num1 = randomNum(10, 999);
   const num2 = randomNum(10, 999);
   eq.textContent = `
-  ${num1} ${randomOperator} ${num2}`;
+  ${num1} ${operator} ${num2}`;
 }
 
-// num1.textContent = randomNum(10, 999);
-// num2.textContent = randomNum(10, 999);
-// console.log(num1.textContent, num2.textContent);
-// // If num1 is smaller than num2, only select the `+` or `*` operators
-// if (parseInt(num1.textContent) < parseInt(num2.textContent)) {
-//   console.log(`num1: ${num1.textContent}, num2: ${num2.textContent}`);
-// } else {
-//   return;
-// }
-
-calcValues();
+renderEquation();
 
 // 5. Randomly generate an operator but don't allow operators that lead to negative results
-// Function to create operator that we then call in calcValues() for the operator
+// Function to create operator that we then call in renderEquation() for the operator
 
 // 5. Calculate the value of each randomly generated equation
 
 
 // 6. Generate a new equation when the user clicks on the "Next" button
-btnNext.addEventListener('click', calcValues);
+btnNext.addEventListener('click', renderEquation);
