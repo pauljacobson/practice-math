c3b765c7ed031f749786db4f84068adb4a6cafcb
Paul Jacobson <pljcbsn@gmail.com>
2017-12-18 20:14:02 +0200

    Thinking modular although still trying to figure that out

---
+++ app/assets/scripts/modules/selectOperator.js
@@ -0,0 +1,12 @@
+// export default function selectOperator() {
+//   const randomOperator = operators[Math.floor(Math.random()*operators.length)];
+//   if (randomOperator === '+') {
+//     operator = '\u002B';
+//   } else if (randomOperator === '-') {
+//     operator = '\u002D';
+//   } else if (randomOperator === '*') {
+//     operator = '\u00D7';
+//   } else if (randomOperator === '/') {
+//     operator = '\u00F7';
+//   }
+// }
